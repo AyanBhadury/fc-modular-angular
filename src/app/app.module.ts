a736b763d8464cbe204e3f85332cf0a7f4745c76
AyanBhadury <ayon.bony@gmail.com>
2021-05-26 14:14:14 +0530

    v1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// Import angular-fusioncharts
+import { FusionChartsModule } from 'angular-fusioncharts';
+
+import FusionCharts from "fusioncharts/core";
+import Pie from "fusioncharts/viz/pie2d";
+import FusionTheme from "fusioncharts/themes/es/fusioncharts.theme.fusion";
+
+
+FusionChartsModule.fcRoot(FusionCharts, Pie, FusionTheme);
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FusionChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
